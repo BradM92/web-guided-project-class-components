da2ae6f7b772566fc65e8e0777c16b382c9865e8
Warren C. Longmire <warren.longmire@lambdaschool.com>
2022-01-10 17:13:56 -0500

    lecture work-in-progress

--- followAlong/src/components/GroceryList.js
+++ followAlong/src/components/GroceryList.js
@@ -2,16 +2,29 @@ import React from "react";
 
 import Item from "./Item";
 
-const GroceryList = props => {
-  // for sorting the list based on whether an item has been purchased or not
-  // const sortedList = props.groceries.sort((a, b) => a.purchased - b.purchased);
-  return (
-    <div className="shopping-list">
-      {props.groceries.map(item => (
-        <Item handleToggleItem={props.handleToggleItem} key={item.id} item={item} />
-      ))}
-    </div>
-  );
-};
+// const GroceryList = props => {
+//   // for sorting the list based on whether an item has been purchased or not
+//   // const sortedList = props.groceries.sort((a, b) => a.purchased - b.purchased);
+//   return (
+//     <div className="shopping-list">
+//       {props.groceries.map(item => (
+//         <Item handleToggleItem={props.handleToggleItem} key={item.id} item={item} />
+//       ))}
+//     </div>
+//   );
+// };
+
+class GroceryList extends React.Component {
+  render() {
+    return (
+      <div className="shopping-list">
+         {
+          this.props.groceries.map(item => (
+            <Item handleToggleItem={props.handleToggleItem} key={item.id} item={item} />
+         ))}
+       </div>
+    )
+  }
+}
 
 export default GroceryList;
