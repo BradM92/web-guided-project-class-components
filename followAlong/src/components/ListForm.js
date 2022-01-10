a772f6f55bdc9e6d1f72d652c759a2225451e46e
Warren C. Longmire <warren.longmire@lambdaschool.com>
2022-01-10 14:03:35 -0500

    lecture work-in-progress

--- followAlong/src/components/ListForm.js
+++ followAlong/src/components/ListForm.js
@@ -16,7 +16,6 @@ class ListForm extends React.Component {
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        {/* This is an uncontrolled component 😬 We want it to be controlled by state */}
         <input type="text" name="item" />
         <button>Add</button>
       </form>
