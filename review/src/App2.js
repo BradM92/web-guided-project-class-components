62afaf429818da2fa6815bfbc34326be9b38702e
Warren C. Longmire <warren.longmire@lambdaschool.com>
2022-01-10 13:16:12 -0500

    lecture work-in-progress

--- review/src/App2.js
+++ review/src/App2.js
@@ -10,8 +10,8 @@ import React from 'react';
 class Person extends React.Component {
     render() {
         return(<div>
-            <h1>Hello {props.name}.</h1>
-            <p>Your age is {props.age}.</p>
+            <h1>Hello {this.props.name}.</h1>
+            <p>Your age is {this.props.age}.</p>
         </div>);
     }
 }
