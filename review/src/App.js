9b323471923725be8f4cb53966e5142fbaddb611
brad <bradley.michaels@outlook.com>
2022-01-23 05:24:48 -0500

    first commit

--- review/src/App.js
+++ review/src/App.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React,  {useState} from "react";
 
+const Person = (props) =>{
+  return(
+    <div>
+      <h1>Hello {props.name}</h1>
+      <p>You are {props.age} years old</p>
+      <button onClick={()=>{
+        setState({
+          ...state,
+          name: "Michael"
+        })
+      }}>Press to change name to Adam !</button>
+    </div>
+  )
+}
 const App = () => {
+  const [state,setState] = useState({
+    name: "Brad",
+    age: 30,
+
+  });
+
+  const handleClick = () => {
+    setState({
+      ...state,
+      name: "Adam"
+    });
+  }
   return (
     <div>
-      <h1>Hello Warren</h1>
+      <Person name ={this.state.name} age={this.state.age}/>
+      
     </div>
   );
 };
