973315be228a26a0fbe70e54f25bf920d839bd4e
brad <bradley.michaels@outlook.com>
2022-01-23 05:33:49 -0500

    app2js state set

--- review/src/App2.js
+++ review/src/App2.js
@@ -1,7 +1,28 @@
 import React from 'react'
 
 class App2 extends React.Component{
-
+    constructor() {
+        super();
+        this.state = {
+            name: 'Brad',
+            age: 30,
+        }
+    }
+    render() {
+        const name = "Cynthia";
+        const age = "31";
+        return(
+            <div><p>Hello {this.state.name}</p>
+            <p>Your age is {this.state.age}</p>
+            <button onClick = {() => {
+                this.state({
+                    ...this.state,
+                    name: "Rose",
+                })
+            }}>change name to Rose</button>
+            </div>
+        )
+    }
 }
 
-export App2;
+export default App2;
